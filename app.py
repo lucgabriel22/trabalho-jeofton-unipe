f310c4daecbfbc093e713f7e35e55fc256606dc3
luc <lucasgabriel20182005@gmail.com>
2024-06-02 19:46:37 -0300

    :sparkles: adicionado gitignore e adicionado função de resolução para vídeo

--- app.py
+++ app.py
@@ -1,57 +1,52 @@
 from pytube import YouTube
+from pytube.exceptions import PytubeError
 import tkinter
 import customtkinter
 import os
 
-
 def IniciarDownloadMusica():
     try:
         ytLink = link.get()
-        ytObject = YouTube(ytLink, on_progress_callback=on_progress)
+        ytObject = YouTube(ytLink)
         musica = ytObject.streams.filter(only_audio=True).first()
         file_path = f"{ytObject.title}.mp3"
-        musica.download(output_path='.', filename=file_path)
+        musica.download(output_path='./musicas', filename=file_path)
+        os.rename(file_path, file_path.replace(".mp4", ".mp3"))  # Renomear o arquivo para .mp3
         titulo.configure(text=ytObject.title, text_color='white')
-        ultimalabel.configure(
-            text='Download Concluído com sucesso!', text_color='green')
-    except Exception as e:
-        ultimalabel.configure(text=f'Erro: {str(e)}', text_color='red')
-
+        ultimalabel.configure(text='Download Concluído com sucesso!', text_color='green')
+    except PytubeError as e:
+        ultimalabel.configure(text=f'Erro Pytube: {str(e)}', text_color='red')
 
 def IniciarDownloadVideo():
     try:
         ytLink = link.get()
-        ytObject = YouTube(ytLink, on_progress_callback=on_progress)
-        video = ytObject.streams.get_highest_resolution(on_progress_callback=on_progress)
+        ytObject = YouTube(ytLink)
+        resolucao_video = resolucao_var.get()
+        
+        # Selecionar stream de vídeo e áudio combinados
+        video = ytObject.streams.filter(progressive=True, res=resolucao_video).first()
+        
+        if not video:
+            ultimalabel.configure(text=f'Resolução {resolucao_video} não disponível', text_color='red')
+            return
+        
         file_path = f"{ytObject.title}.mp4"
-        video.download(output_path='.', filename=file_path)
+        video.download(output_path='./videos', filename=file_path)
         titulo.configure(text=ytObject.title, text_color='white')
-        ultimalabel.configure(
-            text='Download Concluído com sucesso!', text_color='green')
-    except Exception as e:
-        ultimalabel.configure(text=f'Erro: {str(e)}', text_color='red')
+        ultimalabel.configure(text='Download Concluído com sucesso!', text_color='green')
+    except PytubeError as e:
+        ultimalabel.configure(text=f'Erro Pytube: {str(e)}', text_color='red')
 
-
-def on_progress(stream, chunk, bytes_remaining):
-    tamanho_total = stream.filesize
-    bytes = tamanho_total - bytes_remaining
-    porcentagem = bytes / tamanho_total * 100
-    por = str(int(porcentagem))
-    barra_progresso2.configure(text=por + '%')
-    barra_progresso2.update()
-    barra_progresso.set(float(porcentagem) / 100)
-
-
-# < ========== Configs Initials ========== >
+# < ========== Configs Iniciais ========== >
 customtkinter.set_appearance_mode('System')
 customtkinter.set_default_color_theme('blue')
 
 app = customtkinter.CTk()
 app.geometry('720x480')
 app.title('Paneleiros Downloader')
+resolucao_var = tkinter.StringVar()
 
-
-# < ========== TITULO DO APP ========== >
+# < ========== TÍTULO DO APP ========== >
 titulo = customtkinter.CTkLabel(app, text='Coloque o link da sua música/vídeo que deseja baixar')
 titulo.pack(padx=10, pady=10)
 
@@ -62,21 +57,17 @@ link.pack()
 ultimalabel = customtkinter.CTkLabel(app, text='')
 ultimalabel.pack()
 
-# < ========== BARRA DE PROGRESSO ========== >
-barra_progresso = customtkinter.CTkLabel(app, text='0%')
-barra_progresso.pack()
-
-barra_progresso2 = customtkinter.CTkProgressBar(app, width=400)
-barra_progresso2.set(0)
-barra_progresso2.pack(padx=10, pady=10)
-
-# <============ BOTÃO DOWNLOAD em formato MP3 ==========>
+# < ========== BOTÃO DOWNLOAD em formato MP3 ========== >
 downloadmusica = customtkinter.CTkButton(app, text='Iniciar Download em MP3', command=IniciarDownloadMusica)
 downloadmusica.pack(padx=10, pady=10)
 
-# <============ BOTÃO DOWNLOAD em formato MP3 ==========>
-downloadvideo = customtkinter.CTkButton(app, text='Iniciar Download em MP3', command=IniciarDownloadVideo)
+# < ========== BOTÃO DOWNLOAD em formato MP4 ========== >
+downloadvideo = customtkinter.CTkButton(app, text='Iniciar Download em MP4', command=IniciarDownloadVideo)
 downloadvideo.pack(padx=10, pady=10)
 
-app.mainloop()
+# < ========== DROPDOWN ========== >
+resolucao_opcoes = ['1080p', '720p', '480p', '360p', '240p']
+resolucao_dropdown = customtkinter.CTkOptionMenu(app, values=resolucao_opcoes, variable=resolucao_var)
+resolucao_dropdown.pack()
 
+app.mainloop()
