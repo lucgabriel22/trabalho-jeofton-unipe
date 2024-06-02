14bdae950d9214adbab047ac8ee9eb812bbdce58
dickson <dicksonoliveirasouza@gmail.com>
2024-06-02 10:46:47 -0300

    alterado configuração do rótulo final / música.

--- app.py
+++ app.py
@@ -10,3 +10,15 @@ def IniciarDownloadMusica():
         ytObject = YouTube(ytLink, on_progress_callback=on_progress)
         musica = ytObject.streams.filter(only_audio=True).first()
         file_path = f"{ytObject.title}.mp3"
+        musica.download(output_patch='.', filename=filepatch)
+        titulo.configure(text=ytObject.title, text_color='white')
+        ultimalabel.configure(text='Download Concluído com sucesso!', text_color='green')
+    except Exception as e:
+        ultimalabel.configure(text=f'Erro: {str(e)}', text_color='red')
+
+
+def IniciarDownloadVideo():
+    try:
+        ytLink = link.get()
+        ytObject = YouTube(ytLink, on_progress_callback=on_progress)
+        video = ytObject.streams.get_highest_resolution()                
